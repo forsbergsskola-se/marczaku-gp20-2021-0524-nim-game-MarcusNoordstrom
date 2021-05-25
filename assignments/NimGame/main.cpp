d7ef32111ce4acd99601752f199f603daccf3714
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-05-25 13:48:44 +0200

    Added multiplayer!

--- assignments/NimGame/main.cpp
+++ assignments/NimGame/main.cpp
@@ -4,23 +4,46 @@
 using namespace std;
 
 bool gameOver = false;
+bool multiplayer = false;
 
 void GameOver(bool playerWon){
     gameOver = true;
-    if(playerWon){
-        cout << "GZ YOU WON!";
+    if(!multiplayer){
+        if(playerWon){
+            cout << "GZ YOU WON!";
+        }
+        else{
+            cout << "You loose....";
+        }
     }
     else{
-        cout << "You loose....";
+        if(playerWon){
+            cout << "Player 1 won!";
+        }
+        else{
+            cout << "Player 2 won!";
+        }
     }
+
 }
 
 int main() {
     Matches matches;
-    bool playerTurn = true;
+    bool playerOneTurn = true;
+
+    cout << "--NimGame-- \nChoose you're mode\n1. 2Players | 2. AgainstAI" << endl;
+
+    int mode;
+    cin >> mode;
+
+    if(mode == 1){
+        multiplayer = true;
+    }
+    else{
+        multiplayer = false;
+    }
 
     cout << "--NimGame-- \nRemove 1-3 from the matches by typing a number between 1-3 and press ENTER\nIF you want to quit, type 9 and press ENTER\n" << endl;
-
 
 
     while(!gameOver){
@@ -28,15 +51,15 @@ int main() {
 
 
 
-        while(playerTurn){
-            //Print matches
+        while(playerOneTurn){
+            cout << "Player 1 turn: ";
             cout << matches.GetVisual() << endl;
 
             cin >> action;
 
             if(action == 9){
                 gameOver = true;
-                playerTurn = !playerTurn;
+                playerOneTurn = !playerOneTurn;
                 break;
             }
 
@@ -48,7 +71,7 @@ int main() {
                     break;
                 }
 
-                playerTurn = !playerTurn;
+                playerOneTurn = !playerOneTurn;
                 break;
             }
 
@@ -56,21 +79,50 @@ int main() {
                 cout << "Invalid input. Try again." << endl;
                 break;
             }
-
-
         }
 
-        while(!playerTurn && !gameOver){
+        while (multiplayer && !playerOneTurn){
+            cout << "Player 2 turn: ";
+            cout << matches.GetVisual() << endl;
+
+            cin >> action;
+
+            if(action == 9){
+                gameOver = true;
+                playerOneTurn = !playerOneTurn;
+                break;
+            }
+
+            if(action > 0 && action < 4){
+                matches.Remove(action);
+
+                if(matches.GetCount() <= 0){
+                    GameOver(false);
+                    break;
+                }
+
+                playerOneTurn = !playerOneTurn;
+                break;
+            }
+
+            if(action < 0 || action > 3 && action != 9){
+                cout << "Invalid input. Try again." << endl;
+                break;
+            }
+        }
+
+        while(!playerOneTurn && !gameOver && !multiplayer){
             int random = rand() % 3 + 1;
             matches.Remove(random);
-            cout << "Enemy removed: " + to_string(random) + " Matches" << endl;
+            cout << "AI turn: ";
+            cout << "removed: " + to_string(random) + " Matches" << endl;
 
             if(matches.GetCount() <= 0){
                 GameOver(true);
                 break;
             }
 
-            playerTurn = true;
+            playerOneTurn = true;
             break;
         }
     }
