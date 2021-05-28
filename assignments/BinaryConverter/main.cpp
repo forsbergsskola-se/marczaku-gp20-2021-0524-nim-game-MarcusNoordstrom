941fc45ca8ff5de108d68098d44593f7e5a28d7c
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-05-28 12:32:24 +0200

    Added bitwise binary converter

--- assignments/BinaryConverter/main.cpp
+++ assignments/BinaryConverter/main.cpp
@@ -1,52 +1,60 @@
 using namespace std;
 #include <iostream>
 
-int main() {
-    cout << "Enter a number" << endl;
-    int number;
-    cin >> number;
+class Binary{
+public:
+    void DecimalToBinaryExtra() {
+        cout << "BITWISE: Enter a number" << endl;
+        int number;
+        cin >> number;
 
-    int binary[8]{0, 0, 0, 0, 0, 0, 0, 0};
-    int i = 0;
-    bool negative = number < 0;
-
-    while(number > 0){
-        int rem = number % 2;
-        number /= 2;
-
-        if(rem == 1){
-            binary[i] = 1;
+        bool zero = false;
+        cout << "0b";
+        for (int i = 7; i >= 0; --i) {
+            int x = number >> i;
+            if (x & 1) {
+                cout << "1";
+                zero = true;
+            }else if(zero)
+                cout << "0";
         }
-        else binary[i] = 0;
-        i += 1;
     }
 
-    if(!negative){
+    void DecimalToBinary(){
+        cout << "Enter a number" << endl;
+        int number;
+        cin >> number;
+
+        int binary[8]{0, 0, 0, 0, 0, 0, 0, 0};
+        int i = 0;
+
+        while(number > 0){
+            int rem = number % 2;
+            number /= 2;
+
+            if(rem == 1){
+                binary[i] = 1;
+            }
+            else binary[i] = 0;
+            i += 1;
+        }
+        cout << "0b";
+
         for(int x = 0; x < 8; x++){
             cout << binary[x];
         }
     }
-    /*else{
-        for (int x = 0; x < 8; x++) {
-            if(binary[x] == 0){
-                binary[x] = 1;
-            }
-            else if(binary[x] == 1){
-                binary[x] = 0;
-            }
-        }
+};
 
-        for(int x = 0; x < 8; x++){
-            if(binary[x] == 1){
-                binary[x] = 0;
-            }
-            else if(binary[x] == 0){
-                binary[x] = 1;
-                break;
-            }
-        }
-    }*/
+int main() {
+    Binary b;
+    b.DecimalToBinary();
+    cout << endl;
+    b.DecimalToBinaryExtra();
 
     return 0;
 }
+
+
+
 
